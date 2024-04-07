c2801ca77a71c38d460103ac8975549e7398212b
pract-react <ramanathinst@gmail.com>
2024-04-07 05:34:15 +0530

    msf

--- newp/src/App.jsx
+++ newp/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
        <h1 className="text-3xl font-bold underline">
-      Hello world! love is
+      Hello world! love is  commmit
     </h1>
         
     </>
